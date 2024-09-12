cffe7d7fbfce4c74f41149a2e7f28b75c2227fd6
mytty870 <otani11ace@icloud.com>
2024-09-12 09:55:54 +0900

    quick start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
-
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-2xl">Hello World</h1>
     </div>
   )
 }
---
+++ src/routes/about.lazy.tsx
@@ -0,0 +1,13 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+
+export const Route = createLazyFileRoute('/about')({
+  component: About,
+})
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  )
+}
